18ba39c200e32f7119afc40a95034e2ae8d21c95
Adrian Galindez <tu_correo_de_github@ejemplo.com>
2025-10-08 13:20:28 +0000

    Add demo app, k8s manifests and CI/CD workflow

--- demo-app/app.py
+++ demo-app/app.py
@@ -1,8 +1,9 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
 class H(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hola desde mi-app en AKS!")
-HTTPServer(('0.0.0.0',8080), H).serve_forever()
+        self.wfile.write(b'Hola desde mi-app')
 
+HTTPServer(('0.0.0.0', 8080), H).serve_forever()
